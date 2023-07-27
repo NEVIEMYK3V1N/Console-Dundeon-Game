dd3ce3337d765a9f19cff76e033231063c80ca8b
NEVIE_K3V1N <87032954+NEVIEMYK3V1N@users.noreply.github.com>
2023-07-26 21:42:16 -0400

    Update cell.h

--- cell.h
+++ cell.h
@@ -3,12 +3,13 @@
 class Cell {
     Floor* root_floor;
  protected:
-    int pos_row;
-    int pos_col;
+    //int pos_row;
+    //int pos_col;
     char notation;
     int cell_type;
     bool player_walkable;
     bool entity_spawnable;
+    int index_in_map;
  public:
     virtual void render_cell() const = 0;
 };
